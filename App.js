ac4d37d7e8f0b9f716dea59561178f5ee763526e
LauraOrtiz5 <lortizclua@gmail.com>
2024-02-23 19:24:45 +0100

    Title component creation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
@@ -19,13 +19,13 @@ export default function App() {
   }
 
   return (
-    <LinearGradient colors={['#ddb52f','#4e0319']} style={styles.rootScreen}>
+    <LinearGradient colors={['#4e0319', '#ddb52f']} style={styles.rootScreen}>
       <ImageBackground 
         source={require('./assets/images/background.png')}
         resizeMode="cover"
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}>
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,25 @@
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Title from '../components/Title';
 
 function GameScreen () {
-    return <Text>Game Screen!</Text>
+    return (
+    <View style={styles.screen}>
+        <Title>Opponent's Guess</Title>
+        {/*GUESS*/}
+        <View>
+            <Text>Higher or lower?</Text>
+            {/*+ -*/}
+        </View>
+        {/*<View>LOG RUNS</View>*/}
+    </View>
+    );
 }
 
 export default GameScreen;  
+
+const styles = StyleSheet.create({
+    screen: {
+        flex:1,
+        padding: 24
+    }
+});
