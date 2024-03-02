f6905a6eb3898098d775c206736a0a1324ec9e47
LauraOrtiz5 <lortizclua@gmail.com>
2024-03-02 16:22:25 +0100

    Rounds log on Game Screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Alert } from 'react-native';
+import { StyleSheet, Text, View, Alert, FlatList } from 'react-native';
 import { useState, useEffect } from 'react';
 
 import Title from '../components/ui/Title';
@@ -27,6 +27,7 @@ let maxBoundary = 100;
 function GameScreen({ userNumber, onGameOver }) {
     const initialGuess = generateRandomBetween(1, 100, userNumber);
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
+    const [guessRound, setGuessRound] = useState([initialGuess]);
 
     useEffect(() => {
         if (currentGuess === userNumber) {
@@ -55,6 +56,7 @@ function GameScreen({ userNumber, onGameOver }) {
         }
         const newRndNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
         setCurrentGuess(newRndNumber);
+        setGuessRound(prevGuessRound => [newRndNumber, ...prevGuessRound]);
     }
 
     return (
@@ -78,7 +80,13 @@ function GameScreen({ userNumber, onGameOver }) {
                     </View>
                 </View>
             </Card>
-            {/*<View>LOG RUNS</View>*/}
+            <View>
+                <FlatList
+                    data={guessRound}
+                    renderItem={(itemData) => <Text>{itemData.item}</Text>}
+                    keyExtractor={(item) => item}
+                />
+            </View>
         </View>
     );
 }
