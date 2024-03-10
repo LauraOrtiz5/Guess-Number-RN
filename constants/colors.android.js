d46b2bcb40d6e3c7fb9be95c2cdf24c58e1c62ec
LauraOrtiz5 <lortizclua@gmail.com>
2024-03-10 12:14:16 +0100

    Platform API styling

---
+++ constants/colors.android.js
@@ -0,0 +1,9 @@
+const Colors = {
+    primary500: '#BF9BB3',
+    primary600: '#B287A3',
+    primary700: '#7E4E60',
+    primary800: '#482728',
+    accent500: '#AFCBFF',
+};
+
+export default Colors;
